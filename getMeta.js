6c93d93229cf8e5ec87699c286a5bffb5d3c45d6
Alpha <versari@pm.me>
2019-06-19 16:29:17 +0200

    We are sucesfully fetching books into redux

--- getMeta.js
+++ getMeta.js
@@ -6,20 +6,20 @@ var fs = require("fs");
 var directoryPath = path.join(__dirname, "../../Dropbox/writing/equanimity");
 const bookDirectories = [];
 const books = [];
+var files = fs.readdirSync(directoryPath);
+files.forEach(function(file) {
+  if (fs.lstatSync(path.join(directoryPath, file)).isDirectory()) {
+    bookDirectories.push(file);
+  }
+});
+console.log(bookDirectories);
+bookDirectories.map(function(dir) {
+  var file = fs.readFileSync(path.join(directoryPath, dir) + "/meta.json");
+  var temp = {};
+  temp[dir] = JSON.parse(file);
+  books.push(temp);
+});
 
 exports.getMeta = function() {
-  var files = fs.readdirSync(directoryPath);
-  files.forEach(function(file) {
-    if (fs.lstatSync(path.join(directoryPath, file)).isDirectory()) {
-      bookDirectories.push(file);
-    }
-  });
-  console.log(bookDirectories);
-  bookDirectories.map(function(dir) {
-    var file = fs.readFileSync(path.join(directoryPath, dir) + "/meta.json");
-    books.push(JSON.parse(file));
-  });
-  console.log(books);
-
   return books;
 };
